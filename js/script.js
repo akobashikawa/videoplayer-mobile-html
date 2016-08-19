38fccec64ed26079d548b98ca7343b2f908a2cbb
Rulo Kobashikawa <akobashikawa@gmail.com>
2016-08-19 12:23:28 -0500

    Implement catalog carousel

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-$('.carousel').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
+$(document).on('ready', function() {
+
+  $('.carousel').slick({
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    nextArrow: '<i class="next-arrow fa fa-chevron-right fa-2x"></i>',
+    prevArrow: '<i class="prev-arrow fa fa-chevron-left fa-2x"></i>'
+  });
+
 });
